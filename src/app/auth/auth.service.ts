927a8be4792dc316eda767e74116d8e00238aa79
ApexBugFinder <ApexBugFinder@gmail.com>
2018-06-15 18:38:09 -0700

    updated code so that upon login and registering of user when the ui is loading it uses redux managed slices to control ui elements like the loading spinner, this just implements the triggers/calls to the store dispatch

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,14 @@
-import { User } from "./user.model";
-import { AuthData } from "./auth-data.model";
+import { User } from './user.model';
+import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs/Subject';
 import { Injectable } from '@angular/core';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 import { AngularFireAuth} from 'angularfire2/auth';
-import { TrainingService } from "../training/training.service";
-import { UIService } from "../shared/ui.service";
+import { TrainingService } from '../training/training.service';
+import { UIService } from '../shared/ui.service';
+import { Store } from '@ngrx/store';
+// imports everything from the app.Reducer file and can be access through fromApp
+import * as fromApp from '../app.reducer';
 
 
 
@@ -16,11 +19,12 @@ export class AuthService {
     private isAuthenticated = false;
 
     constructor(
-        private router: Router, 
-        private afAuth: AngularFireAuth, 
-        private trainingService: TrainingService, 
-        
-        private uiService: UIService) {}
+        private router: Router,
+        private afAuth: AngularFireAuth,
+        private trainingService: TrainingService,
+        private uiService: UIService,
+        // added access to global store
+        private store: Store<{ui: fromApp.State}>) {}
 
     initAuthListener() {
         this.afAuth.authState.subscribe(user => {
@@ -37,12 +41,20 @@ export class AuthService {
         })
     }
     registerUser(authData: AuthData) {
-        this.uiService.loadingStateChanged.next(true);
+        // uiService
+        // this.uiService.loadingStateChanged.next(true);
+        // using redux managed slice to dispatch/tell all parties that the state has changed
+        this.store.dispatch({type: 'START_LOADING'});
         this.afAuth.auth.createUserWithEmailAndPassword(
             authData.email,
             authData.password
         ).then(result => {
-            this.uiService.loadingStateChanged.next(false);
+            // using uiService managed state
+            // this.uiService.loadingStateChanged.next(false);
+            // using redux's ui managed state in the appReducer store, everyone listening
+            // to the store will hear the false boolean emitted by the case STOP_LOADING case
+            // in the form of the isLoading property
+            this.store.dispatch({type: 'STOP_LOADING'});
             console.log(result);
         }).catch(error => {
             this.uiService.loadingStateChanged.next(false);
@@ -51,20 +63,21 @@ export class AuthService {
     }
 
 login(authData: AuthData) {
-    this.uiService.loadingStateChanged.next(true);
+    // this.uiService.loadingStateChanged.next(true);
+    this.store.dispatch({type: 'START_LOADING'});
     this.afAuth.auth.signInWithEmailAndPassword(authData.email, authData.password)
         .then(result => {
-            this.uiService.loadingStateChanged.next(false);
+            // this.uiService.loadingStateChanged.next(false);
+            this.store.dispatch({type: 'STOP_LOADING'});
             console.log(result);
-        }).catch(error=> {
-            this.uiService.loadingStateChanged.next(false);
+        }).catch(error => {
+            // this.uiService.loadingStateChanged.next(false);
+            this.store.dispatch({type: 'STOP_LOADING'});
             this.uiService.showSnackBar(error.message, null, 3000);
-        });;
-  
+        });
 }
 logout() {
     this.afAuth.auth.signOut();
-    
 }
 
 
